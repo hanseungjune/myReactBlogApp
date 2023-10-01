116e1910e14860429912a804ad78d61cb784995a
한승준 <tmdwns950428@naver.com>
2023-10-01 14:00:17 +0900

    feat: firebase 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import firebase from "firebaseApp";
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
