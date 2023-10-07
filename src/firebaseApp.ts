d17dddc105ea4f770b0f9ee01d73917f0e6e3165
한승준 <tmdwns950428@naver.com>
2023-10-07 15:52:04 +0900

    feat: firestore 세팅하기

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,5 +1,6 @@
 import { FirebaseApp, getApp, initializeApp } from "firebase/app";
 import "firebase/auth";
+import { getFirestore } from "firebase/firestore" 
 
 export let app: FirebaseApp;
 
@@ -19,5 +20,5 @@ try {
 }
 
 const firebase = initializeApp(firebaseConfig);
-
+export const db = getFirestore(app);
 export default firebase;
